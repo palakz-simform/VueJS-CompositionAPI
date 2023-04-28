6439f29f6d9e5745ede07c1a148e955eca8549db
Palak Zalavadia <palak.z@simformsolutions.com>
2023-04-28 20:02:55 +0530

    Removed alert from login

--- car-showroom/src/stores/user.js
+++ car-showroom/src/stores/user.js
@@ -46,8 +46,6 @@ export const useUserStore = defineStore('user', () => {
                         localStorage.setItem('token', `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`)
                         localStorage.setItem('loggedIn', true)
                     }
-
-                    alert("Logged In Successfully!!")
                     router.push({
                         name: 'home'
                     })

a6909134c343995a52d0b5502453bf5bcb23dfc3
Palak Zalavadia <palak.z@simformsolutions.com>
2023-05-03 16:24:08 +0530

    Changes in style.css file import

--- car-showroom/src/main.js
+++ car-showroom/src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import router from './router/index'
 import App from './App.vue'
 import NavBar from './components/NavBar.vue'
-import '../public/style.css'
 
 const pinia = createPinia()
 const app = createApp(App)
